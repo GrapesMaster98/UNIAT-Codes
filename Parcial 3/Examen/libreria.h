827cd02e9dff731208531e8d953346a23692e5f3
GrapesMaster98 <oliverklassen@protonmail.com>
2023-11-29 16:36:35 -0600

    Examen

---
+++ Parcial 3/Examen/libreria.h
@@ -0,0 +1,8 @@
+//
+// Created by Alumnos on 29/11/2023.
+//
+
+#ifndef UNIAT_CODES_LIBRERIA_H
+#define UNIAT_CODES_LIBRERIA_H
+
+#endif //UNIAT_CODES_LIBRERIA_H
---
+++ Parcial 3/Examen/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*}
+ * CREAR UN SISTEMA DE POLIZAS DE SEGURO
+ *
+ * EL PROGRAMA DEBERÁ DARLE UN MENÚ AL USUARIO PARA CREAR UNA POLIZA
+ *
+ * CLASE PADRE: nombre, edad (se calcula con base a la fecha de nacimiento), CURP, fecha de nacimiento
+ *
+ * CASA:
+ * Direccion, tamaño, año de construccion
+ * */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
