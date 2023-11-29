b334fbc425083b6151ba5fadb7cdfd81823e3ac8
GrapesMaster98 <oliverklassen@protonmail.com>
2023-11-29 16:47:55 -0600

    Created classes

--- Parcial 3/Examen/libreria.h
+++ Parcial 3/Examen/libreria.h
@@ -1,8 +1,20 @@
-//
-// Created by Alumnos on 29/11/2023.
-//
+#include <iostream>
+#include <string>
 
 #ifndef UNIAT_CODES_LIBRERIA_H
 #define UNIAT_CODES_LIBRERIA_H
+
+class Poliza {
+public:
+    std::string nombre;
+    int edad;
+    std::string nacimiento;
+    std::string curp;
+};
+
+//TODO: Fill with data
+class Casa : Poliza {};
+class Carro : Poliza {};
+class Vida : Poliza {};
 
 #endif //UNIAT_CODES_LIBRERIA_H
